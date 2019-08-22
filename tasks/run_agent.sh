6af07261a756979b06f19b03826a972c15fc94ff
Adrian Parreiras Horta <adrian.parreiras-horta@puppet.com>
2019-08-22 10:36:24 -0700

    Initial commit

---
+++ tasks/run_agent.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# shellcheck disable=SC2154,SC2034,SC1090
+
+declare PT__installdir
+source "$PT__installdir/deploy_pe/files/common.sh"
+PUPPET_BIN=/opt/puppetlabs/bin
+
+(( EUID == 0 )) || fail "This utility must be run as root"
+
+lockfile="$("${PUPPET_BIN}/puppet" config print agent_catalog_run_lockfile)"
+retries=5
+wait_time=300
+
+# Sleep in increments of 1 until either the lockfile is gone or we reach $wait_time
+while [[ -e $lockfile ]] && (( wait_time > 0 )); do
+  (( wait_time-- ))
+  sleep 1
+done
+
+# Fail if the lock still exists
+[[ -e $lockfile ]] && fail "Agent lockfile $lockfile still exists after waiting $wait_time seconds"
+
+# Run Puppet until there are no changes, otherwise fail
+for ((i = 0; i < retries; i++)); do
+  "${PUPPET_BIN}/puppet" agent -t >/dev/null && {
+    success '{ "status": "Successfully ran Puppet agent" }'
+  }
+done
+
+fail "Failed to run Puppet agent in $retries attempts"
